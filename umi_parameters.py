ba85ef009d92f2419f66caef3366d52face1c985
rlhjansen <rlh.jansen@outlook.com>
2017-06-13 14:25:34 +0200

    haha zwart wint

--- umi_parameters.py
+++ umi_parameters.py
@@ -30,10 +30,10 @@ class UMI_parameters:
         ## TODO for students: REPLACE MINIMUM_DEGREES AND MAXIMUM_DEGREES FOR EACH INDIVIDUAL JOINT, THEY ARE NOT THE SAME FOR
         # SHOULDER, ELBOW, AND WRIST
         self.joint_ranges = {
-            "Riser"     : [0.0, maximum_height_change_this],
-            "Shoulder"  : [mimimum_degrees_change_this, maximum_degrees_change_this],
-            "Elbow"     : [mimimum_degrees_change_this, maximum_degrees_change_this],
-            "Wrist"     : [mimimum_degrees_change_this, maximum_degrees_change_this],
+            "Riser"     : [0.0, 0.925],
+            "Shoulder"  : [-0.09, 0.09],
+            "Elbow"     : [-0.18, 0.11],
+            "Wrist"     : [-0.11, 0.11],
             "Gripper"   : [0, 0.05]
         }
 
